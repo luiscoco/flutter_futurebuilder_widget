7e18252fc7bbcb1bc02d138a1b0a8b229914a52b
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2023-11-10 11:01:57 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_futurebuilder_widget/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_futurebuilder_widget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('FutureBuilder Example'),
+        ),
+        body: MyFutureBuilderWidget(),
+      ),
+    );
+  }
+}
+
+class MyFutureBuilderWidget extends StatelessWidget {
+  Future<String> fetchData() async {
+    // Simulating an asynchronous operation, e.g., fetching data from an API
+    await Future.delayed(Duration(seconds: 2));
+    return "Hello, FutureBuilder!";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<String>(
+      future: fetchData(),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+            return Text('Press button to start.');
+          case ConnectionState.active:
+          case ConnectionState.waiting:
+            return Text('Awaiting result...');
+          case ConnectionState.done:
+            if (snapshot.hasError)
+              return Text('Error: ${snapshot.error}');
+            return Text('Result: ${snapshot.data}');
+        }
+      },
+    );
+  }
+}
